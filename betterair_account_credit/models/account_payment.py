e264e1266966b0c1220f147830b225f8b3827e50
Zeno Nanon <zna@odoo.com>
2022-05-31 06:59:34 -0700

    [MOD] Reformat

--- betterair_account_credit/models/account_payment.py
+++ betterair_account_credit/models/account_payment.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
+
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError, RedirectWarning
 from odoo.tools.misc import formatLang, format_date
 import logging
+
 INV_LINES_PER_STUB = 9
+
 _logger = logging.getLogger(__name__)
 class AccountPayment(models.Model):
     _inherit = "account.payment"
+
     # Modified to allow credit to show on printed check document
     def _check_make_stub_pages(self):
         """ The stub is the summary of paid invoices. It may spill on several pages, in which case only the check on
             first page is valid. This function returns a list of stub lines per page.
         """
         self.ensure_one()
+
         def prepare_vals(invoice, partials):
             number = ' - '.join([invoice.name, invoice.ref] if invoice.ref else [invoice.name])
+
             if invoice.is_outbound():
                 invoice_sign = 1
                 partial_field = 'debit_amount_currency'
             else:
                 invoice_sign = -1
                 partial_field = 'credit_amount_currency'
+
             if invoice.currency_id.is_zero(invoice.amount_residual):
                 amount_residual_str = '-'
             else:
@@ -33,6 +40,7 @@ class AccountPayment(models.Model):
                 'amount_paid': formatLang(self.env, invoice_sign * sum(partials.mapped(partial_field)), currency_obj=self.currency_id),
                 'currency': invoice.currency_id,
             }
+
         # Decode the reconciliation to keep only invoices.
         term_lines = self.line_ids.filtered(lambda line: line.account_id.internal_type in ('receivable', 'payable'))
         invoices = (term_lines.matched_debit_ids.debit_move_id.move_id + term_lines.matched_credit_ids.credit_move_id.move_id)\
@@ -45,6 +53,7 @@ class AccountPayment(models.Model):
                 amount_map[counterpart_line.move_id] = amount
         invoices += credits
         invoices = invoices.sorted(lambda x: x.invoice_date_due or x.date)
+
         # Group partials by invoices.
         invoice_map = {invoice: self.env['account.partial.reconcile'] for invoice in invoices}
         for partial in term_lines.matched_debit_ids:
@@ -82,6 +91,7 @@ class AccountPayment(models.Model):
                         stub_line = prepare_vals(invoice, partials)
                         stub_line['amount_paid'] = formatLang(self.env, -amount, currency_obj=self.currency_id)
                         stub_lines += [stub_line]
+
         # Crop the stub lines or split them on multiple pages
         if not self.company_id.account_check_printing_multi_stub:
             # If we need to crop the stub, leave place for an ellipsis line
@@ -98,7 +108,10 @@ class AccountPayment(models.Model):
                     num_stub_lines = INV_LINES_PER_STUB
                 stub_pages.append(stub_lines[i:i + num_stub_lines])
                 i += num_stub_lines
+
         return stub_pages
+
+
     # Modified to add credit notes to list view of paid invoices
     @api.depends('move_id.line_ids.matched_debit_ids', 'move_id.line_ids.matched_credit_ids')
     def _compute_stat_buttons_from_reconciliation(self):
@@ -113,9 +126,11 @@ class AccountPayment(models.Model):
             self.reconciled_statement_ids = False
             self.reconciled_statements_count = 0
             return
+
         self.env['account.move'].flush()
         self.env['account.move.line'].flush()
         self.env['account.partial.reconcile'].flush()
+
         self._cr.execute('''
             SELECT
                 payment.id,
@@ -167,6 +182,7 @@ class AccountPayment(models.Model):
                             bills += counterpart_line.move_id
                 pay.reconciled_bill_ids += bills 
                 pay.reconciled_bills_count = len(pay.reconciled_bill_ids)
+
         self._cr.execute('''
             SELECT
                 payment.id,
@@ -193,6 +209,7 @@ class AccountPayment(models.Model):
             'payment_ids': tuple(stored_payments.ids)
         })
         query_res = dict((payment_id, statement_ids) for payment_id, statement_ids in self._cr.fetchall())
+
         for pay in self:
             statement_ids = query_res.get(pay.id, [])
             pay.reconciled_statement_ids = [(6, 0, statement_ids)]
